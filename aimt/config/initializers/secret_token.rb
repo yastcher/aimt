4254e71c0bd426d9a2f180d17f68e699f50ec68c
Yury <info@aimt.pro>
2013-10-17 13:13:15 +0400

    OK

---
+++ aimt/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aimt::Application.config.secret_key_base = 'c09693e5eddb4471fcfd20ac7359089566523dae98621ca54693b6de103aec01f949a04c44f5b554d713b61f7494d5dadc11494cc586299468b5227d92a01591'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aimt::Application.config.secret_key_base = 'ceb21f911e1f6d2e8d2364dfaed56a14a7519d3dbd7088d3cfd924d12422b4b0f27fd2bd9cc7463d188defa77e68964043d1d9eeb2a3ef5ce66bfa01eac364d1'
